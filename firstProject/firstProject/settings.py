81c529a3eef2796c81610aaaf77d102dba97981b
canislatranscoxus <canislatranscoxus@gmail.com>
2022-10-14 15:25:56 -0500

    allowed hosts

--- firstProject/firstProject/settings.py
+++ firstProject/firstProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^x#=7swvdln#1281es$-tio3c%@8n9#f+nv$xe0s^6s^=)vze0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
